55e0c69846c60a6085ebc29b8cd2327b9f2f4ab4
Mikuhann <ng.kano@gmail.com>
2019-03-05 13:47:36 +0300

    add button component with styled components add title for product component add some basic markup for product list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
+import Title from './Title';
 import Product from './Product'
 
 export default class ProductList extends Component {
+  state: {
+    products: []
+  };
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+      <React.Fragment>
+        <div className='py-5'>
+          <div className="container">
+            <Title name='our'
+            title='products'/>
+            <div className="row">
+
+            </div>
+          </div>
+        </div>
+        {/*<Product />*/}
+      </React.Fragment>
     );
   }
 }
